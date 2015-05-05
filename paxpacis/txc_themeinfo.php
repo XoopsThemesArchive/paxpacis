bce388c06cc14c53d856b8f05c33f55759d73778
mambax7 <mambax7@gmail.com>
2015-05-05 09:23:45 -0400

    Original files

---
+++ paxpacis/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "paxpacis";
+	$txc_themeinfo["author"] = "δ֪";
+	$txc_themeinfo["author_url"] = "#";
+	$txc_themeinfo["detail_url"] = "http://downloads.myxoops.de";
+	$txc_themeinfo["download_url"] = "http://downloads.myxoops.de";
+?>
